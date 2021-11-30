5f54cdca504b15a5c9c8870e69f0af44417b16a1
murawakimitsuhiro <mitsuhiromurawaki@gmail.com>
2021-11-30 18:04:25 +0900

    [add] alias vim ssh

---
+++ zsh/user-extension/.zsh.after/alias.zsh
@@ -0,0 +1,5 @@
+# ssh configを開く
+alias vimssh='vim ~/.ssh/config'
+
+# 秘密鍵で複合する
+alias decless='(){openssl smime -decrypt -binary -in $1 -inform DER -inkey ~/.Private/private-key.pem | less}'
--- zsh/user-extension/.zsh.after/decless.zsh
+++
@@ -1,1 +0,0 @@
-alias decless='(){openssl smime -decrypt -binary -in $1 -inform DER -inkey ~/.Private/private-key.pem | less}'
