95919f3484a98d94bb55d121e4f7990384e1e10c
murawakimitsuhiro <mitsuhiromurawaki@gmail.com>
2022-02-02 13:29:11 +0900

    [add] github create command ghrc

--- zsh/user-extension/.zsh.after/ghqpeco.zsh
+++ zsh/user-extension/.zsh.after/ghqpeco.zsh
@@ -3,3 +3,17 @@
 alias g='cd $(ghq root)/$(ghq list | peco)'
 alias gopen='hub browse $(ghq list | peco | cut -d "/" -f 2,3)'
 
+# github repository create
+ghcr() {
+    echo "Repository name: " && read name;
+    echo "Repository description: " && read description;
+    gh repo create ${name} --private -d ${description}
+    ghq get git@github.com:murawakimitsuhiro/${name}.git
+    cd ~/src/github.com/murawakimitsuhiro/${name}
+    gh gitignore
+    gsed -e '$ a .DS_Store' -i .gitignore
+    ga
+    git commit -m "Initial commit"
+    gpsh
+}
+
