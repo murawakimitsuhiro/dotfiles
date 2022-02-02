0c8fe46a037b610cfe254a276e639e426ebeabfb
murawakimitsuhiro <mitsuhiromurawaki@gmail.com>
2022-02-02 12:39:23 +0900

    [add] relgoin alias

--- zsh/user-extension/.zsh.after/alias.zsh
+++ zsh/user-extension/.zsh.after/alias.zsh
@@ -3,3 +3,6 @@ alias vimssh='vim ~/.ssh/config'
 
 # 秘密鍵で複合する
 alias decless='(){openssl smime -decrypt -binary -in $1 -inform DER -inkey ~/.Private/private-key.pem | less}'
+
+# relogin
+alias relogin='exec $SHELL -l'
--- zsh/user-extension/.zsh.after/ghqpeco.zsh
+++ zsh/user-extension/.zsh.after/ghqpeco.zsh
@@ -1,5 +1,5 @@
 
 # ghq + peco
 alias g='cd $(ghq root)/$(ghq list | peco)'
-alias gh='hub browse $(ghq list | peco | cut -d "/" -f 2,3)'
+alias gopen='hub browse $(ghq list | peco | cut -d "/" -f 2,3)'
 
